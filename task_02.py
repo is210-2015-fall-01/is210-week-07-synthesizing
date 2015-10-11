ac52290acb4a976a96cc34443445fead8c3b22eb
Saulat Majid <solyraja@hotmail.com>
2015-10-11 04:27:07 +0000

    Submitting assignments

---
+++ task_02.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Creates a program for login and authentication"""
+
+
+import authentication
+import getpass
+USER = 'mike'
+
+
+def login(username, max_attempts=3):
+    """This function loops through user input to authenticate password
+
+    Args:
+        It takes 2 arguments, username and max_attempts, with 3 as default max.
+
+    Returns:
+        A boolean
+    """
+
+    authenticated = False
+    attempt = 0
+    while (attempt < max_attempts) or (authenticated == False):
+        myval = getpass.getpass(prompt='Enter password:  ')
+        attempt += 1
+        authenticated = authentication.authenticate(username, myval)
+
+    return authenticated
+
+login(USER)
