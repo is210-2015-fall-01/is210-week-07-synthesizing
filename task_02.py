18be8096f6dc3c51c1c9b9c998ec36352cd20570
Vagrant User <cj.fernanders1986@gmail.com>
2015-10-12 16:52:34 +0000

    tried again

--- task_02.py
+++ task_02.py
@@ -2,32 +2,36 @@
 # -*- coding: utf-8 -*-
 """Task 02"""
 
+
 import authentication
 import getpass
 
 
 def login(username, maxattempts=3):
-    """the structure of a login
+    """A login page.
 
-    Args:
-         A string representing the username of the user attempting to log in.
-         An integer represent the maximum number of prompted attempts.
+    Agrs:
+         The username of the user attempting to log in.
+         The maximum number of prompted attempts.
 
     Returns:
         Return True if the user has successfully authenticated
         False if they exceed that maximum number of failed attempts.
+
+
     """
 
     authenticated = False
-    numattempts = 0
-    incorrect = 'Incorrect username or password. You have {} attempts left.'
-    while not authenticated and numattempts < maxattempts:
+    counter = 0
+    warning = 'Incorrect username or password. You have {} attempts left.'
+
+    while not authenticated and counter < maxattempts:
         password = getpass.getpass('Please enter your password:')
+
         if authentication.authenticate(username, password) is True:
             authenticated = True
 
         else:
-            numattempts += 1
-            print incorrect.format(maxattempts - numattempts)
-
-        return authenticated
+            counter += 1
+            print warning.format(maxattempts - password)
+    return authenticated
